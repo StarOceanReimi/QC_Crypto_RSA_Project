656a6dec002daab575a69abfb9d9eed4399f3be9
Leester Mei <leesterm@gmail.com>
2016-11-20 17:00:42 -0500

    Alternate sieve

--- QuadraticSieve.java
+++ QuadraticSieve.java
@@ -4,6 +4,16 @@ import java.util.Random;
 import java.util.HashMap;
 
 public class QuadraticSieve{
+  public static final BigInteger ONE = BigInteger.ONE;
+  public static final BigInteger ZERO = BigInteger.ZERO;
+  public static HashMap<Integer,BigInteger> tptp = new HashMap<>();
+  
+  public static BigInteger gcd(BigInteger a,BigInteger b){
+    if(b.equals(BigInteger.ZERO))
+      return a;
+    else
+      return gcd(b,a.mod(b));
+  }
   public static int[] primesLessThanErastothenes(int b){
     int[] primes = new int[b+1];
     for(int i = 2;i<primes.length;i++){
@@ -78,29 +88,32 @@ public class QuadraticSieve{
     }
     return x;
   }
-  
-  public static void main(String[] args){  
-    BigInteger ONE = BigInteger.ONE;
-    BigInteger ZERO = BigInteger.ZERO;
-    //BigInteger n = new BigInteger("613054310726032886180943888436325837702226698886723435429939101863");
-    BigInteger n = new BigInteger("62113");
-    int b = 37;
-    //int b = 100;
-    //BigInteger interval = new BigInteger("1000000");//Interval should be large enough to find pi(B)+1 b-smooth numbers
-    BigInteger interval = new BigInteger("100");
-    //BigInteger start = new BigInteger("782977848170708394135693691175755");
-    BigInteger start = sqrt(n,1100);
-    HashMap<BigInteger, ArrayList<Integer>> b_smooth_candidates = new HashMap<BigInteger, ArrayList<Integer>>();
+  public static ArrayList reduceFactorBase(BigInteger n,int b){
     int[] primes = primesLessThanErastothenes(b);//Find all primes less than B bound
     ArrayList factorbase = new ArrayList();//Reduce the prime factorbase
     for(int i=2;i<primes.length;i++){
       if(primes[i] == 0){
         if(legendre(n,BigInteger.valueOf(i)) == 1){
           factorbase.add(i);
-          //System.out.println(i);
+          //System.out.print(i+" ");
         }
       }
     }
+    return factorbase;
+  }
+  public static void dumbQuadraticSieve(){
+    //BigInteger n = new BigInteger("613054310726032886180943888436325837702226698886723435429939101863");
+    BigInteger n = new BigInteger("62113");
+    int b = 37;
+    //int b = 100;
+    //BigInteger interval = new BigInteger("10000000");//Interval should be large enough to find pi(B)+1 b-smooth numbers
+    BigInteger interval = new BigInteger("100");
+    //BigInteger start = new BigInteger("782977848170708394135693691175755");
+    BigInteger start = sqrt(n,1100);
+    //System.out.println("Starting: "+start);
+    HashMap<BigInteger, ArrayList<Integer>> b_smooth_candidates = new HashMap<BigInteger, ArrayList<Integer>>();
+    ArrayList factorbase = reduceFactorBase(n,b);
+    
     //For every prime in the prime factorbase
     for(int p=0;p<factorbase.size();p++){
       int prime = (int)factorbase.get(p);
@@ -151,6 +164,7 @@ public class QuadraticSieve{
         }
       }
     }
+    //Check if x^2-n is b-smooth
     for(BigInteger i:b_smooth_candidates.keySet()){
       BigInteger candidate = i.pow(2);
       candidate = candidate.subtract(n);
@@ -162,7 +176,73 @@ public class QuadraticSieve{
         }
       }
       if(candidate.compareTo(ONE) == 0)
-        System.out.println(i+" "+factors);
+        System.out.println("Found B-Smooth "+i+" "+factors);
     }
+  }
+  public static void smarterQuadraticSieve(BigInteger n,int b,BigInteger interval){
+    //https://cr.yp.to/factorization/smoothparts-20040510.pdf
+    BigInteger start = sqrt(n,1100).add(ONE);
+    ArrayList<Integer> factorbase = reduceFactorBase(n,b);
+    
+    long Z = 1;
+    for(int p: factorbase){
+      Z *= p;
+    }
+    for(long i = 0;interval.compareTo(BigInteger.valueOf(i)) > 0;i++){
+      BigInteger x = start.add(BigInteger.valueOf(i));
+      x = x.multiply(x);
+      x = x.subtract(n);
+      x = x.mod(n);
+      
+      
+      BigInteger Zmodx = BigInteger.valueOf(Z).mod(x);
+      int j = 1;
+      while(x.compareTo(tptp.get(j)) >= 0){
+        j++;
+      }
+      BigInteger y = Zmodx;
+      for(int r=0;r<j;r++){
+        y = y.multiply(y).mod(x);
+      }
+      BigInteger gcd = gcd(x,y);
+      
+      //System.out.print(start.add(BigInteger.valueOf(i))+" x: "+x+" y: "+y+"||"+gcd);
+      //if(x.divide(gcd).equals(ONE))
+      //  System.out.println("<--This is b-smooth");
+      //else
+      //  System.out.println();
+      //System.out.println(start.add(BigInteger.valueOf(i))+": "+x.divide(gcd));
+      //
+      
+      if(x.divide(gcd).equals(ONE))
+        System.out.println(start.add(BigInteger.valueOf(i))+"|x: "+x);
+      
+    }
+  }
+  public static void main(String[] args){  
+    tptp.put(1,BigInteger.valueOf(4));
+    tptp.put(2,BigInteger.valueOf(16));
+    tptp.put(3,BigInteger.valueOf(256));
+    tptp.put(4,BigInteger.valueOf(65536));
+    tptp.put(5,new BigInteger("4294967296"));
+    tptp.put(6,new BigInteger("18446744073709551616"));
+    tptp.put(7,new BigInteger("340282366920938463463374607431768211456"));
+    tptp.put(8,new BigInteger("115792089237316195423570985008687907853269984665640564039457584007913129639936"));
+      
+    BigInteger n = new BigInteger("613054310726032886180943888436325837702226698886723435429939101863");
+    int b = 200000;
+    BigInteger interval = new BigInteger("5000000");//Interval should be large enough to find pi(B)+1 b-smooth numbers
+    
+    /*
+    BigInteger n = new BigInteger("1237891986662");
+    int b = 10000;
+    BigInteger interval = new BigInteger("10000000");
+    */
+    /*
+    BigInteger n = new BigInteger("62113");
+    int b = 37;
+    BigInteger interval = new BigInteger("100");
+    */
+    smarterQuadraticSieve(n,b,interval);
   }
 }
